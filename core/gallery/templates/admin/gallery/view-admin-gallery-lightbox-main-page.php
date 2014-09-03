d51097f65c84d425c9835f22eac48b9cd6a593ca
AlexanderZon <alexisanderson@ovi.com>
2014-09-03 11:39:59 -0430

    Gallery Main Page Fix

--- core/gallery/templates/admin/gallery/view-admin-gallery-lightbox-main-page.php
+++ core/gallery/templates/admin/gallery/view-admin-gallery-lightbox-main-page.php
@@ -195,6 +195,7 @@
     var_dump($gldb->getGalleries());
 
     var_dump(get_posts(array(
+                    'post_status' => 'any',
                     'post_type' => 'gl_gallery'
                     )));
     
