2b530091e62441cf50bf165fc0f0b2dfd1ad8391
AlexanderZon <alexisanderson@ovi.com>
2014-09-05 01:58:19 -0430

    Items Add Bug Fix

--- core/gallery/classes/admin/class-admin-menu-items.php
+++ core/gallery/classes/admin/class-admin-menu-items.php
@@ -29,6 +29,10 @@ class ClassAdminMenuItems extends ClassAdminMenuParent{
 		
 			$data = $_POST;
 
+			var_dump($_FILE);
+
+			die();
+
 			$file = $_FILE['image'];
 				
 			$item = array(
@@ -107,7 +111,7 @@ class ClassAdminMenuItems extends ClassAdminMenuParent{
 			$uploadedfile = $file;
 			$upload_overrides = array( 'test_form' => false );
 			$movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
-			if ( $movefile ):
+			if ( !isset($movefile['error']) ):
 			    return $movefile;
 			else:
 			    return false;
