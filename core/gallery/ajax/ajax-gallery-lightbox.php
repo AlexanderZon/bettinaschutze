ed48f8f4af0342c45f5f104ea4ac3dc82304621a
AlexanderZon <alexisanderson@ovi.com>
2014-09-08 06:07:22 -0430

    Gallery Lightbox DB fix in ajax

--- core/gallery/ajax/ajax-gallery-lightbox.php
+++ core/gallery/ajax/ajax-gallery-lightbox.php
@@ -21,11 +21,11 @@
 	$response['items'] = $items;
 
 	for( $i = 0 ; $i < count($response['items']) ; $i++ ):
-		$src = wp_get_attachment_thumb_url($response['items'][$i]['post_excerpt']);
+		$src = wp_get_attachment_url($response['items'][$i]['post_excerpt']);
 		$response['items'][$i]['src'] = $src;
 		$photos = $gldb->getPhotos($response['items'][$i]['ID'], 'publish');
 		for( $j = 0 ; $j < count($photos) ; $j++ ):
-			$photos[$j]['src'] = wp_get_attachment_thumb_url($photos[$j]['post_excerpt']);
+			$photos[$j]['src'] = wp_get_attachment_url($photos[$j]['post_excerpt']);
 		endfor;
 		$response['items'][$i]['photos'] = $photos;
 	endfor;
