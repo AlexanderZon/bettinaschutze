650066fce8ed47f4c7b5a1b9b9ec4ebb33e483a0
AlexanderZon <alexisanderson@ovi.com>
2014-09-08 03:01:19 -0430

    Ajax Testingization

--- core/gallery/ajax/ajax-gallery-lightbox.php
+++ core/gallery/ajax/ajax-gallery-lightbox.php
@@ -1,3 +1,19 @@
 <?php
 
 	require_once( "../../../../../../wp-config.php" );
+
+	global $wpdb;
+
+	$fivesdrafts = $wpdb->get_results( 
+		"
+		SELECT ID, post_title 
+		FROM $wpdb->posts
+		WHERE post_status = 'draft' 
+			AND post_author = 5
+		"
+	);
+
+	foreach ( $fivesdrafts as $fivesdraft ) 
+	{
+		echo $fivesdraft->post_title;
+	}
