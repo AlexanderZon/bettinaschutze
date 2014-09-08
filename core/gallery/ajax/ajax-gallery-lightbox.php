5b40df4b42c8419d18a16a221c65b5fcb56f4383
AlexanderZon <alexisanderson@ovi.com>
2014-09-08 05:43:38 -0430

    Gallery Lightbox DB Testing in ajax

--- core/gallery/ajax/ajax-gallery-lightbox.php
+++ core/gallery/ajax/ajax-gallery-lightbox.php
@@ -21,8 +21,10 @@
 	$response['items'] = $items;
 
 	for( $i = 0 ; $i < count($response['items']) ; $i++ ):
+		$src = wp_get_attachment_thumb_url($item['post_excerpt']);
 		$photos = $gldb->getPhotos($response['items'][$i]['ID'], 'publish');
 		$response['items'][$i]['photos'] = $photos;
+		$response['items'][$i]['src'] = $src;
 	endfor;
 
 	header('Content-Type: application/json');
