0df3bd692e39265bb392ff4bb3d484a3312f8c70
AlexanderZon <alexisanderson@ovi.com>
2014-09-08 03:28:57 -0430

    Gallery Lightbox DB Testing in ajax

--- core/gallery/ajax/ajax-gallery-lightbox.php
+++ core/gallery/ajax/ajax-gallery-lightbox.php
@@ -4,28 +4,10 @@
 
 	require_once( "../class.db.php" );
 
-	echo "Test<br>";
-
 	global $wpdb, $gldb;
-
-	var_dump($_REQUEST);
-
-	echo "Test<br>";
-
-	echo "Test<br>";
 
 	$data = $_POST;
 
-	echo "Test<br>";
-
 	$gallery = $gldb->getGallery($data['id']);
-
-	echo "Test<br>";
 	
 	echo $gallery->post_title;
-
-	var_dump($gldb);
-
-	var_dump($gallery);
-
-	echo "Test<br>";
