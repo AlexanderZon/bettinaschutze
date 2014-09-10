39c72fa7a2b38f2d845bb36305ae8019024a6978
AlexanderZon <alexisanderson@ovi.com>
2014-09-10 01:55:58 -0430

    Video Gallery Fix

--- js/fs_gllaery.js
+++ js/fs_gllaery.js
@@ -7,6 +7,47 @@
 	License : MIT License / GPL License
 */
 
+var video_lightbox = function(){
+		console.log("Video click");
+		var item = function(item){
+			//console.log(item);
+			return '<a id="fancybox_'+item.ID+'" class="fancybox fancybox_'+item.ID+'" rel="fancybox_'+item.ID+'" href="'+item.src+'" title="'+item.post_title+'"><img src="'+item.src+'" alt="" height="80"/></a>';
+		}
+
+		var photo = function(photo, ID){
+			//console.log(photo);
+			return '<a id="fancybox_'+photo.ID+'" class="fancybox fancybox_'+ID+'" rel="fancybox_'+ID+'" href="'+photo.src+'" title="'+photo.post_title+'"><img src="'+photo.src+'" style="display:none" alt="" /></a>';
+		}
+
+		var data = {
+			'id': 383
+		};
+
+		var html = '';
+
+		$.post('/wp-content/themes/bettinaschutze/core/gallery/ajax/ajax-gallery-lightbox.php', data, function(response) {
+
+			for(var i = 0 ; i < response.items.length ; i++ ){
+				html += item(response.items[i]);
+				for(var j = 0 ; j < response.items[i].photos.length ; j++ ){
+					html += photo( response.items[i].photos[j],  response.items[i].ID);
+				}
+			}
+			$('.fs_thmb_viewport ').append(html);
+
+			for(var i = 0 ; i < response.items.length ; i++ ){
+				$('.fancybox_'+response.items[i].ID).fancybox({
+		          	helpers: {
+		              	title : {
+		                  	type : 'float'
+		              	}
+		          	}
+		      	});
+			}
+			return html;
+		});
+	}
+
 jQuery.fn.fs_gallery = function(fs_options) {
 
 	var gallery_lightbox = function(){
@@ -258,10 +299,10 @@ jQuery.fn.fs_gallery = function(fs_options) {
 
 	});
 		
-	$('#item-menu-73').click(function(e){
+	$('#item-menu-73 a').click(function(e){
 		e.preventDefault();
 		console.log("Click");
-		video_lightbox();
+		var html = video_lightbox();
 	});
 	
 	$('.fs_thmb_viewport').width($(window).width()-$fs_title.width()-58)
