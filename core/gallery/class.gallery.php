98604dd0e7c1c9957f7fcd2ed8dd139edd1cc9bf
AlexanderZon <alexisanderson@ovi.com>
2014-09-10 04:14:08 -0430

    Back to HTTPS fix

--- core/gallery/class.gallery.php
+++ core/gallery/class.gallery.php
@@ -12,7 +12,7 @@ if(!class_exists("GalleryLightbox")){
 
 			spl_autoload_register( array($this, 'autoload') );
 
-			include_once( $this->plugin_path() . '/class.db.php' );
+			//include_once( $this->plugin_path() . '/class.db.php' );
 
 			//$this->initializeAdmin();
 
