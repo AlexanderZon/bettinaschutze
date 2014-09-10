0bc672647622ddf5cd7821e820011eff93e29a56
AlexanderZon <alexisanderson@ovi.com>
2014-09-10 06:15:12 -0430

    fs_gllaery Video Fix

--- js/fs_gllaery.js
+++ js/fs_gllaery.js
@@ -91,7 +91,7 @@ jQuery.fn.fs_gallery = function(fs_options) {
 
 		var video = function(video){
 			console.log(video);
-			return '<a id="fancybox_'+video.ID+'" class="fancybox fancybox_'+video.ID+'" rel="fancybox_'+video.ID+'" href="'+video.content+'" title="'+video.post_title+'"><img src="'+video.src+'" alt="" /></a>';
+			return '<a id="fancybox_'+video.ID+'" class="fancybox fancybox_'+video.ID+'" rel="fancybox_'+video.ID+'" href="'+video.post_content+'" title="'+video.post_title+'"><img src="'+video.src+'" alt="" /></a>';
 		}
 
 		var data = {
