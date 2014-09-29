bb42b32e72dee7497bc0bbd195f456eceba30ce5
AlexanderZon <alexisanderson@ovi.com>
2014-09-29 12:22:50 -0430

    ajax bio bug fix

--- core/gallery/ajax/ajax-contact.php
+++ core/gallery/ajax/ajax-contact.php
@@ -6,10 +6,6 @@
 
 	global $wpdb;
 
-	$data = $_GET;
-
-	$bio = $wpdb->get_row("SELECT * FROM $wpdb->posts WHERE ID = ".$data['id'], ARRAY_A);
-
 	$logo = get_theme_option("logo");
 
 	$html = '<img src="'.$logo.'" ><br>
