fe95628061dcd0d3cf6f7bd77c66b4446e6ada30
AlexanderZon <alexisanderson@ovi.com>
2014-09-29 14:15:22 -0430

    Mail ajax fix debuggin

--- core/gallery/ajax/ajax-mail.php
+++ core/gallery/ajax/ajax-mail.php
@@ -8,24 +8,7 @@
 
 	$logo = get_theme_option("logo");
 
-	$html = '<img src="'.$logo.'" ><br>
-			<div style="width:500px;text-align:justify;">
-				<form id="contact-form" action="" method="post">
-				<table>
-					<tr><td><label>Name</label></td><td><input type="text" name="name"/></td></tr>
-					<tr><td><label>Email</label></td><td><input type="email" name="email" /></td></tr>
-					<tr><td><label>Subject</label></td><td><input type="text" name="subject" /></td></tr>
-					<tr><td><label>Message</label></td><td><textarea name="message"></textarea> </td></tr>
-					<tr><td colspan="2"><input type="submit" value="Submit"></td></tr>
-				</form>
-				<script>
-					$("#contact-form").on("submit", function(e){
-						e.preventDefault();
-						
-					});
-				</script>
-			</div>
-	';
+	$html = 'respuesta email';
 	echo $html;
 
 ?>
