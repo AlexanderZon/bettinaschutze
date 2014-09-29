6444d92bfc73f367e0dfafe21071c758271b5494
AlexanderZon <alexisanderson@ovi.com>
2014-09-29 12:08:13 -0430

    Ajax html return bug fix

--- core/gallery/ajax/ajax-bio.php
+++ core/gallery/ajax/ajax-bio.php
@@ -13,7 +13,7 @@
 	$logo = get_theme_option("logo");
 
 	$html = '<img src="'.$logo.'" ><br>
-			<div style="width:50%">'.$bio['post_content'].'</div>
+			<div style="width:500px">'.$bio['post_content'].'</div>
 	';
 	echo $html;
 
