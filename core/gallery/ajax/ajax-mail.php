afbf7662822b19c6766078471f2377bdfe9f0415
AlexanderZon <alexisanderson@ovi.com>
2014-09-30 15:21:03 -0430

    Mail Configuration bug fix

--- core/gallery/ajax/ajax-mail.php
+++ core/gallery/ajax/ajax-mail.php
@@ -10,7 +10,8 @@
 	$data = $_POST;
 
 	$headers = 'From: '.$data['name'].' <'.$data['email'].'>' . '\r\n';
-	$mail = mail( 'amontenegro.sistemas@gmail.com' , $data['subject'] , $data['message'] );
+	$mail = mail( 'amontenegro.sistemas@gmail.com' , 'Juan lopez se baña en la cabaña' , 'fredo godofredo me gusta el yogurt' );
+	//$mail = mail( 'amontenegro.sistemas@gmail.com' , $data['subject'] , $data['message'] );
 	//$mail = wp_mail( 'alex_100aleman@hotmail.com', $data['subject'], $data['message'] , $headers );
 	
 	if($mail):
