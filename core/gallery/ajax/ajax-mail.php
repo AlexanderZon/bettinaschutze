caefc55d68cd1cc23021a8a9a2eed5a98cf4f334
AlexanderZon <alexisanderson@ovi.com>
2014-09-30 15:00:43 -0430

    Mail Configuration fix

--- core/gallery/ajax/ajax-mail.php
+++ core/gallery/ajax/ajax-mail.php
@@ -8,7 +8,7 @@
 	global $wpdb;
 
 	$data = $_POST;
-	return echo json_encode($data);
+	return json_encode($data);
 	$headers = 'From: '.$data['name'].' <'.$data['email'].'>' . '\r\n';
 	$mail = mail( 'alex_100aleman@hotmail.com' , $data['subject'] , $data['message'] );
 	//$mail = wp_mail( 'alex_100aleman@hotmail.com', $data['subject'], $data['message'] /*, $headers*/ );
