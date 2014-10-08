f5bf7c7543ed67db3e7a964c639b05b72e2b8bde
AlexanderZon <alexisanderson@ovi.com>
2014-10-08 10:28:49 -0430

    Contact info fix

--- header.php
+++ header.php
@@ -81,6 +81,7 @@
         <span>bettina@bettinaschutze.com</span>
         <span><b>Phone: </b> 0055 11 98114 2119 </span>
         <span><b>Agency: </b> 0055 11 30428684 </span>
+        <span><b>Germany: </b> 0049 (0) 1725238027 </span>
     </div>
     <div class="header_wrapper container">
        
