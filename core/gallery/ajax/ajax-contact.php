4cdaedce7ca4eeb5e221d3d1244280dba5b60e0f
AlexanderZon <alexisanderson@ovi.com>
2014-10-14 11:50:39 -0430

    phpmailer port bug fix

--- core/gallery/ajax/ajax-contact.php
+++ core/gallery/ajax/ajax-contact.php
@@ -62,13 +62,13 @@
 				<script>
 					$("#contact-form").on("submit", function(e){
 						e.preventDefault();
-						$("#contact-form-container").html("Enviando...");
 						var data = {
 							"name": $("#contact-form-name").val(),
 							"email": $("#contact-form-email").val(),
 							"subject": $("#contact-form-subject").val(),
 							"message": $("#contact-form-message").val(),
 						}
+						$("#contact-form-container").html("Enviando...");
 						console.log(data);
 						$.post("/wp-content/themes/bettinaschutze/core/gallery/ajax/ajax-mail.php", data, function(response) {
 							console.log(response);
