d1d098b5b1f2d07de0a459ff81d6cbf9c2cbdc3c
AlexanderZon <alexisanderson@ovi.com>
2014-10-14 10:52:37 -0430

    phpmailer bug fix

--- core/gallery/ajax/ajax-mail.php
+++ core/gallery/ajax/ajax-mail.php
@@ -106,14 +106,14 @@
 	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'smtp.gmail.com;smtp2.example.com';  // Specify main and backup SMTP servers
+	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
 	$mail->Username = 'amontenegro.sistemas@gmail.com';                 // SMTP username
 	$mail->Password = '23498535';                           // SMTP password
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 465;                                    // TCP port to connect to
 
-	$mail->From = 'from@example.com';
+	$mail->From = 'amontenegro.sistemas@gmail.com';
 	$mail->FromName = 'Mailer';
 	$mail->addAddress('theguitarplayer.am@gmail.com', 'Alexis Montenegro');     // Add a recipient
 	$mail->addAddress('robertdacorte@gmail.com', 'Robert Dacorte');               // Name is optional
