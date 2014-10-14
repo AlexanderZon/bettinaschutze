5f5dbb6033ec906c753c494fa814f3fd5ddc7b9b
AlexanderZon <alexisanderson@ovi.com>
2014-10-14 11:17:49 -0430

    phpmailer port bug fix

--- core/gallery/ajax/ajax-mail.php
+++ core/gallery/ajax/ajax-mail.php
@@ -107,10 +107,10 @@
 	//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+	$mail->Host = 'smtp.gallardodesigner.com.br';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'amontenegro.sistemas@gmail.com';                 // SMTP username
-	$mail->Password = '23498535';                           // SMTP password
+	$mail->Username = 'robert@gallardodesigner.com.br';                 // SMTP username
+	$mail->Password = 'robe4102';                           // SMTP password
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
 
