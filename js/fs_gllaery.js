c63bcacf929080bd661391982eec274144fa7cd5
AlexanderZon <alexisanderson@ovi.com>
2014-11-11 09:26:00 -0430

    Video liughtbox fix

--- js/fs_gllaery.js
+++ js/fs_gllaery.js
@@ -150,7 +150,7 @@ jQuery.fn.fs_gallery = function(fs_options) {
 		}
 
 		var video = function(video){
-			//console.log(video);
+			console.log(video);
 			return '<div style="display:inline-block;height:100px;margin-left:5px;margin-right:5px;width:100px"><a id="fancybox_'+video.ID+'" class="fancybox fancybox-media fancybox_'+video.ID+'" rel="fancybox_'+video.ID+'" href="'+video.post_content+'" title="'+video.post_title+'"><img src="'+video.thumb+'" alt="" height="100"/></a></div>';
 		}
 
@@ -242,8 +242,8 @@ jQuery.fn.fs_gallery = function(fs_options) {
 					openEffect  : 'none',
 					closeEffect : 'none',
 					helpers : {
-						media : {}
-					}
+             			media: true
+         			},
 		      	}, function(){
 			/*
 					$('.una-clase-que-no-se-repita').slick({
