2c66e22a2ba092e078c2a3db153cc9cd61df0fff
AlexanderZon <alexisanderson@ovi.com>
2014-11-13 09:19:49 -0430

    nicescroll adapter fs_gllaery fix

--- js/fs_gllaery.js
+++ js/fs_gllaery.js
@@ -329,7 +329,7 @@ jQuery.fn.fs_gallery = function(fs_options) {
 	          	}
 	      	});
 		$('#fancybox_bio_'+num).click();
-		$(".fancybox-inner").nicescroll({cursorborder:"",cursorcolor:"#00F",boxzoom:true});
+		$(".fancybox-inner").niceScroll({cursorborder:"",cursorcolor:"#ff5474",boxzoom:true})
 	}
 
 	//Set Variables
